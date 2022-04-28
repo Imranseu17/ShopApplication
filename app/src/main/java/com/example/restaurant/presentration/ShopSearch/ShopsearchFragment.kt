e3e3d1649e44f2d873279ac7ac7d358d34f4fd33
imran <imran1995bd@gmail.com>
2022-04-28 14:44:36 +0600

    findNearby restaurant and search with keyword is completed

--- app/src/main/java/com/example/restaurant/presentration/ShopSearch/ShopsearchFragment.kt
+++ app/src/main/java/com/example/restaurant/presentration/ShopSearch/ShopsearchFragment.kt
@@ -65,7 +65,7 @@ class ShopsearchFragment : Fragment(), ShopsAdapter.ShopItemListener {
 
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                     setResult(s.toString())
-                    adapter.filter.filter(s)
+                    //adapter.filter.filter(s)
                     Handler().postDelayed(Runnable
                     {
                         searchHistoryList.add(s.toString())
